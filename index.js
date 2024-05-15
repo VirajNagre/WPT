9c3313272f7251efb1bd46890120392524b2bc99
jitendra <choudharyjitendra125@gmail.com>
2024-05-16 00:51:04 +0530

    jitendra

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ app.get("/",(req,res)=>{
 app.get("/shantanu",(req,res) =>{
     res.send("shantanu")
     })
+app.get("/jitendra",(req,res) =>{
+    res.send("jitendra")
+    })
 app.listen(5000,()=>{console.log("server running")});
