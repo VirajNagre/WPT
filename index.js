3e1d8d0d53e01e3b6b1f76993b1543f389a170da
viraj <virajngr@gmail.com>
2024-05-18 09:15:45 +0530

    DB connected

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 import express from 'express';
-import { client } from './utility/dbUtils.js';
+import { conn } from './utility/dbUtils.js';
+
+// let conn;
 
 const app = express();
 
+async function run() {
+    // try {
+    //   // Connect the client to the server	(optional starting in v4.7)
+    //   await client.connect();
+    //   // Send a ping to confirm a successful connection
+    //   await client.db("admin").command({ ping: 1 });
+    //   console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // } finally {
+    //   // Ensures that the client will close when you finish/error
+    // //   await client.close();
+    // console.log("ouauhdi");
+    // }
+    try {
+        // Connect to the MongoDB server
+        conn = await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        // If MongoDB connection fails, close the Express server
+        console.log('Exiting...');
+        process.exit(1);
+    }
+}
+
+
 app.get("/",(req,res)=>{
     console.log("endpoint reached");
+
     res.send("ok");
 })
-
 
 app.get("/shantanu",(req,res) =>{
     res.send("shantanu")
@@ -17,14 +44,8 @@ app.get("/jitendra",(req,res) =>{
     })
 
 app.listen(5000,async()=>{
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-    console.log("server running")});
+        // run().catch(console.dir);
+        // conn.
+        console.log("server running")
+    }
+);
--- utility/dbUtils.js
+++ utility/dbUtils.js
@@ -3,18 +3,18 @@ import {MongoClient, ServerApiVersion} from "mongodb";
 const uri = "mongodb+srv://root:cdac@projectcluster.envnu1p.mongodb.net/?retryWrites=true&w=majority&appName=ProjectCluster";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-export const client = new MongoClient(uri, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
 });
-
+let conn;
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    conn = await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -23,4 +23,6 @@ async function run() {
     await client.close();
   }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
+
+export {conn}; 
